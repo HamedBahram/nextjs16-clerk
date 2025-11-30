f6f602dd9eac36be5756f8ffcbba612c1d89f9e5
Hamed Bahram <64147793+HamedBahram@users.noreply.github.com>
2025-11-29 20:20:11 -0500

    Initial commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-32'>
+      <div className='container'>
+        <h1 className='text-3xl font-semibold'>NextJs 16 + shadcn</h1>
+      </div>
+    </section>
+  )
+}
