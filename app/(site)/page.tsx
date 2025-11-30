dea9c273036fa76731a6190ca9a623403ef67103
Hamed Bahram <64147793+HamedBahram@users.noreply.github.com>
2025-11-29 20:20:11 -0500

    Initial commit
    Change package manager to bun
    
    Add clerk
    
    Update packages
    
    bump react & nextjs packages
    
    bump react & nextjs package
    
    Update packages
    
    minor update

---
+++ app/(site)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section className='py-32'>
+      <div className='container max-w-7xl'>
+        <h1 className='text-3xl font-semibold'>nextjs 16 + clerk</h1>
+      </div>
+    </section>
+  )
+}
